951db2416a5445a357e6dbb4af1b83678a621f7e
yairhdz24 <yairhdz107@gmail.com>
2024-03-07 22:10:02 -0600

    Add dotenv package and configuration files

--- PetPlace Back/db/db.js
+++ PetPlace Back/db/db.js
@@ -9,16 +9,33 @@
 // });
 
 // module.exports = pool;
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   user: 'doadmin', // usuario de digital ocean
+//   host: 'db-postgresql-pet-place-do-user-15684287-0.c.db.ondigitalocean.com', // host de la base de datos
+//   database: 'defaultdb', // nombre de la bd
+//   password: 'AVNS_0NPsl0fNS_JrUBydmH5', // Reemplaza con tu contraseña
+//   port: 25060, 
+//   ssl: {
+//     rejectUnauthorized: false 
+//   }
+// });
+
+// module.exports = pool;
+
+require('dotenv').config();
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'doadmin', // usuario de digital ocean
-  host: 'db-postgresql-pet-place-do-user-15684287-0.c.db.ondigitalocean.com', // host de la base de datos
-  database: 'defaultdb', // nombre de la bd
-  password: 'AVNS_0NPsl0fNS_JrUBydmH5', // Reemplaza con tu contraseña
-  port: 25060, 
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
   ssl: {
-    rejectUnauthorized: false 
+    rejectUnauthorized: false
   }
 });
 
