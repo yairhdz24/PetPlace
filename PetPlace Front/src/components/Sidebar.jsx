a8da6e510fe71bd092daef7c4b3df2e33fa933f2
yairhdz24 <yairhdz107@gmail.com>
2024-03-16 01:47:41 -0600

    Cambio de directorio - elimine Carpeta Shared

---
+++ PetPlace Front/src/components/Sidebar.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { RiRestaurantFill, RiFileList3Fill, RiContactsBook2Fill, RiHistoryFill } from "react-icons/ri";
+
+const Sidebar = (props) => {
+    const { showMenu } = props;
+    const location = useLocation();
+
+    // Estado local para la pestaña activa
+    const [activeTab, setActiveTab] = useState('/');
+
+    // Actualiza la pestaña activa cuando cambia la ubicación
+    useEffect(() => {
+        setActiveTab(location.pathname);
+    }, [location.pathname]);
+
+    const handleSetActiveTab = (tab) => {
+        setActiveTab(tab);
+    };
+
+    return (
+        <div
+            className={`bg-yellow-400 fixed lg:left-0 top-0 w-28 h-full flex flex-col justify-between py-4 rounded-tr-xl rounded-br-xl
+                                                                        z-50 transition-all ${showMenu ? "left-0" : "-left-full"}`}
+        >
+            <div>
+                <ul className='pl-4'>
+                    <li>
+                        {/* <img
+                            src="splash1.png"
+                            alt="Alitas"
+                            className="h-full w-full pt-3 pl-3 pr-3"
+                        /> */}
+                        <h1 className='text-xl font-Lilita_One text-blue-400 uppercase text-center'>{"PEt"} <br /> {"Place"}</h1>
+                    </li>
+                    <li className={`bg-gray-100 p-4 mb-2 rounded-tl-xl rounded-bl-xl group transition-colors ${activeTab === '/' ? 'bg-purple-400' : ''}`}>
+                        <Link to='/' className={`group-hover:bg-purple-400 p-4 flex justify-center rounded-xl group-hover:text-white transition-colors ${activeTab === '/' ? 'text-white' : 'text-purple-400'}`} onClick={() => handleSetActiveTab('/')}>
+                            <RiRestaurantFill className='text-2xl' />
+                        </Link>
+                    </li>
+                    <li className={`bg-gray-100 p-4 mb-2 rounded-tl-xl rounded-bl-xl group transition-colors ${activeTab === '/pedidos' ? 'bg-purple-400' : ''}`}>
+                        <Link to='/pedidos' className={`group-hover:bg-purple-400 p-4 flex justify-center rounded-xl group-hover:text-white transition-colors ${activeTab === '/pedidos' ? 'text-white' : 'text-purple-400'}`} onClick={() => handleSetActiveTab('/pedidos')}>
+                            <RiFileList3Fill className='text-2xl' />
+                        </Link>
+                    </li>
+                    <li className={`bg-gray-100 p-4 mb-2 rounded-tl-xl rounded-bl-xl group transition-colors ${activeTab === '/clientes' ? 'bg-purple-400' : ''}`}>
+                        <Link to='/clientes' className={`group-hover:bg-purple-400 p-4 flex justify-center rounded-xl group-hover:text-white transition-colors ${activeTab === '/clientes' ? 'text-white' : 'text-purple-400'}`} onClick={() => handleSetActiveTab('/clientes')}>
+                            <RiContactsBook2Fill className='text-2xl' />
+                        </Link>
+                    </li>
+                    <li className={`bg-gray-100 p-4 mb-2 rounded-tl-xl rounded-bl-xl group transition-colors ${activeTab === '/historialpedidos' ? 'bg-purple-400' : ''}`}>
+                        <Link to='/historialpedidos' className={`group-hover:bg-purple-400 p-4 flex justify-center rounded-xl group-hover:text-white transition-colors ${activeTab === '/historialpedidos' ? 'text-white' : 'text-purple-400'}`} onClick={() => handleSetActiveTab('/historialpedidos')}>
+                            <RiHistoryFill className='text-2xl' />
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
